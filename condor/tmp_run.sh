bd6871b5d0690b36e3d7a0a022678fefa69bf843
Dragos Velicanu <velicanu@mit.edu>
2016-10-27 21:18:45 -0400

    condor fix

--- condor/tmp_run.sh
+++ condor/tmp_run.sh
@@ -18,8 +18,11 @@ echo 1 > /mnt/hadoop/cms/store/user/velicanu/locks/${1}.txt
 echo ./$5 `head -n${nline} $2 | tail -n1` ${nline}.root ${arglist} 1 ${mbfile}
 ./$5 `head -n${nline} $2 | tail -n1` ${nline}.root ${arglist} 1 ${mbfile}
 echo $@
+rm L2L3VsPtEtaBinned_ak3PF.root
 if [[ `ls -trs *.root | tail -n1 | awk '{print $1}'` -gt 20 ]]; then # for files with more than 0 events inside
   mv `ls -tr *.root | tail -n1` $3
+else
+  ls -trsh *.root 
 fi
 ls | grep -v .out | grep -v .err | grep -v .log | grep -v _condor_stdout | grep -v _condor_stderr | xargs rm -rf
 
