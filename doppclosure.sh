a6560fd3de89435a8ea09f2629854e9577d6ddff
Dragos Velicanu <velicanu@mit.edu>
2017-02-28 09:45:59 -0500

    update pp closure

--- doppclosure.sh
+++ doppclosure.sh
@@ -8,12 +8,12 @@ fi
 echo running closure histograms
 for i in ${@:7}
 do
-  ./ffgamma.exe /export/d00/scratch/dav2105/ztrees/g.Pythia8_Photon_pp502_TuneCUETP8M1-HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_v3-v1_forest_v1.root ppmc 0 20 $1 $2 $3 $i $4 $5 $6 &
-  ./ffgamma.exe /export/d00/scratch/dav2105/ztrees/g.Pythia8_Photon_pp502_TuneCUETP8M1-HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_v3-v1_forest_v1.root ppmc 20 60 $1 $2 $3 $i $4 $5 $6 &
-  ./ffgamma.exe /export/d00/scratch/dav2105/ztrees/g.Pythia8_Photon_pp502_TuneCUETP8M1-HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_v3-v1_forest_v1.root ppmc 60 100 $1 $2 $3 $i $4 $5 $6 &
-  ./ffgamma.exe /export/d00/scratch/dav2105/ztrees/g.Pythia8_Photon_pp502_TuneCUETP8M1-HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_v3-v1_forest_v1.root ppmc 100 200 $1 $2 $3 $i $4 $5 $6 &
-wait
+  ./ffgamma.exe /export/d00/scratch/dav2105/ztrees/g.Pythia8_Photon_pp502_TuneCUETP8M1-HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_minjet.root ppmc 0 20 $1 $2 $3 $i $4 $5 $6 &
+  ./ffgamma.exe /export/d00/scratch/dav2105/ztrees/g.Pythia8_Photon_pp502_TuneCUETP8M1-HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_minjet.root ppmc 20 60 $1 $2 $3 $i $4 $5 $6 &
+  ./ffgamma.exe /export/d00/scratch/dav2105/ztrees/g.Pythia8_Photon_pp502_TuneCUETP8M1-HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_minjet.root ppmc 60 100 $1 $2 $3 $i $4 $5 $6 &
+  ./ffgamma.exe /export/d00/scratch/dav2105/ztrees/g.Pythia8_Photon_pp502_TuneCUETP8M1-HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_minjet.root ppmc 100 200 $1 $2 $3 $i $4 $5 $6 &
 done
+wait
 
 
 hadd -f closure_pp_${1}_${2}_${3}_gammaxi${6}.root ppmc_ppmc_*_*.root
