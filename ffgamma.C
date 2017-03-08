10e4741f433807e05c2db90489f13a5e4d651c78
Dragos Velicanu <velicanu@mit.edu>
2017-03-08 01:52:43 -0500

    genhasreco

--- ffgamma.C
+++ ffgamma.C
@@ -451,7 +451,7 @@ void ztree::ffgammajet(std::string outfname, int centmin, int centmax, float pho
         tmpjetpt *= smearFactor;
       }
 //! jet selections
-      bool recoHasGen = (gjetpt[ij]>30 && fabs(gjeteta[ij])<1.6 && acos(cos(gjetphi[ij] - phoPhi[0])) > 7 * pi / 8 ); //true if this recojet has a genjet passing selections
+      // bool recoHasGen = (gjetpt[ij]>30 && fabs(gjeteta[ij])<1.6 && acos(cos(gjetphi[ij] - phoPhi[0])) > 7 * pi / 8 ); //true if this recojet has a genjet passing selections
       bool genHasReco = false; //true if this genjet has a recojet
       
       for(int ijtmp = 0 ; ijtmp < njet ; ijtmp++) {
