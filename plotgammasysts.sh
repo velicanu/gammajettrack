4e09d74ce32c6402d686be3cf0c585e6f2eebe58
Dragos Velicanu <velicanu@mit.edu>
2017-03-08 02:10:02 -0500

    gammaxi yaxis label

--- plotgammasysts.sh
+++ plotgammasysts.sh
@@ -5,13 +5,13 @@
 
 ./ff_systematics.exe gammaxi1_sys.list gammaxi1_sys.root 0
 ./add_sys.exe gammaxi1_sys.root gammaxi1_sys_plot.root
-./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_all 0 all
+./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_all 0 all 1
 ./plot_ff_ratio nominal_xigamma_ratio.root gammaxi1_sys_plot.root sysSummary_result_ratio_all all
 
 ## just JES
 ./ff_systematics.exe gammaxi1_sys.list gammaxi1_sys.root 1
 ./add_sys.exe gammaxi1_sys.root gammaxi1_sys_plot.root
-./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_JES 0 JES
+./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_JES 0 JES 1
 ./plot_ff_ratio nominal_xigamma_ratio.root gammaxi1_sys_plot.root sysSummary_result_ratio_JES JES
 ./plot2ff nominal_xigamma.root systjes0_xigamma.root sysSummary_JES_down 0 JES_down 0
 ./plot2ff nominal_xigamma.root systjes1_xigamma.root sysSummary_JES_up 0 JES_up 0
@@ -22,7 +22,7 @@
 ## just purity
 ./ff_systematics.exe gammaxi1_sys.list gammaxi1_sys.root 2
 ./add_sys.exe gammaxi1_sys.root gammaxi1_sys_plot.root
-./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_purity 0 purity
+./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_purity 0 purity 1
 ./plot_ff_ratio nominal_xigamma_ratio.root gammaxi1_sys_plot.root sysSummary_result_ratio_purity purity
 ./plot2ff nominal_xigamma.root systpurity0_xigamma.root sysSummary_purity_down 0 purity_down 0
 ./plot2ff nominal_xigamma.root systpurity1_xigamma.root sysSummary_purity_up 0 purity_up 0
@@ -32,17 +32,17 @@
 ## just JER
 ./ff_systematics.exe gammaxi1_sys.list gammaxi1_sys.root 3
 ./add_sys.exe gammaxi1_sys.root gammaxi1_sys_plot.root
-./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_JER 0 JER
+./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_JER 0 JER 1
 ./plot_ff_ratio nominal_xigamma_ratio.root gammaxi1_sys_plot.root sysSummary_result_ratio_JER JER
-./plot2ff nominal_xigamma.root systjer_mix_xigamma.root sysSummary_JER 0 JER 0
-./plot2ff nominal_xigamma.root systjer_mix_xigamma.root sysSummary_JER_ratio 0 JER 1
+./plot2ff nominal_xigamma.root systjer_xigamma.root sysSummary_JER 0 JER 0
+./plot2ff nominal_xigamma.root systjer_xigamma.root sysSummary_JER_ratio 0 JER 1
 
 
 
 ## just pes
 ./ff_systematics.exe gammaxi1_sys.list gammaxi1_sys.root 4
 ./add_sys.exe gammaxi1_sys.root gammaxi1_sys_plot.root
-./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_pes 0 pes
+./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_pes 0 pes 1
 ./plot_ff_ratio nominal_xigamma_ratio.root gammaxi1_sys_plot.root sysSummary_result_ratio_pes pes
 ./plot2ff nominal_xigamma.root systpes0_xigamma.root sysSummary_pes_down 0 pes_down 0
 ./plot2ff nominal_xigamma.root systpes1_xigamma.root sysSummary_pes_up 0 pes_up 0
@@ -52,7 +52,7 @@
 ## just trk
 ./ff_systematics.exe gammaxi1_sys.list gammaxi1_sys.root 5
 ./add_sys.exe gammaxi1_sys.root gammaxi1_sys_plot.root
-./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_trk 0 trk
+./plot_ff nominal_xigamma.root gammaxi1_sys_plot.root sysSummary_result_trk 0 trk 1
 ./plot_ff_ratio nominal_xigamma_ratio.root gammaxi1_sys_plot.root sysSummary_result_ratio_trk trk
 ./plot2ff nominal_xigamma.root systtrk_xigamma.root sysSummary_trk 0 trk 0
 ./plot2ff nominal_xigamma.root systtrk_xigamma.root sysSummary_trk_ratio 0 trk 1
--- plotsysts.sh
+++ plotsysts.sh
@@ -5,13 +5,13 @@
 
 ./ff_systematics.exe gammaxi0_sys.list gammaxi0_sys.root 0
 ./add_sys.exe gammaxi0_sys.root gammaxi0_sys_plot.root
-./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_all 0 all
+./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_all 0 all 0
 ./plot_ff_ratio nominal_xijet_ratio.root gammaxi0_sys_plot.root sysSummary_result_ratio_all all
 
 ## just JES
 ./ff_systematics.exe gammaxi0_sys.list gammaxi0_sys.root 1
 ./add_sys.exe gammaxi0_sys.root gammaxi0_sys_plot.root
-./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_JES 0 JES
+./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_JES 0 JES 0
 ./plot_ff_ratio nominal_xijet_ratio.root gammaxi0_sys_plot.root sysSummary_result_ratio_JES JES
 ./plot2ff nominal_xijet.root systjes0_xijet.root sysSummary_JES_down 0 JES_down 0
 ./plot2ff nominal_xijet.root systjes1_xijet.root sysSummary_JES_up 0 JES_up 0
@@ -22,7 +22,7 @@
 ## just purity
 ./ff_systematics.exe gammaxi0_sys.list gammaxi0_sys.root 2
 ./add_sys.exe gammaxi0_sys.root gammaxi0_sys_plot.root
-./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_purity 0 purity
+./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_purity 0 purity 0
 ./plot_ff_ratio nominal_xijet_ratio.root gammaxi0_sys_plot.root sysSummary_result_ratio_purity purity
 ./plot2ff nominal_xijet.root systpurity0_xijet.root sysSummary_purity_down 0 purity_down 0
 ./plot2ff nominal_xijet.root systpurity1_xijet.root sysSummary_purity_up 0 purity_up 0
@@ -32,17 +32,17 @@
 ## just JER
 ./ff_systematics.exe gammaxi0_sys.list gammaxi0_sys.root 3
 ./add_sys.exe gammaxi0_sys.root gammaxi0_sys_plot.root
-./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_JER 0 JER
+./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_JER 0 JER 0
 ./plot_ff_ratio nominal_xijet_ratio.root gammaxi0_sys_plot.root sysSummary_result_ratio_JER JER
-./plot2ff nominal_xijet.root systjer_mix_xijet.root sysSummary_JER 0 JER 0
-./plot2ff nominal_xijet.root systjer_mix_xijet.root sysSummary_JER_ratio 0 JER 1
+./plot2ff nominal_xijet.root systjer_xijet.root sysSummary_JER 0 JER 0
+./plot2ff nominal_xijet.root systjer_xijet.root sysSummary_JER_ratio 0 JER 1
 
 
 
 ## just pes
 ./ff_systematics.exe gammaxi0_sys.list gammaxi0_sys.root 4
 ./add_sys.exe gammaxi0_sys.root gammaxi0_sys_plot.root
-./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_pes 0 pes
+./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_pes 0 pes 0
 ./plot_ff_ratio nominal_xijet_ratio.root gammaxi0_sys_plot.root sysSummary_result_ratio_pes pes
 ./plot2ff nominal_xijet.root systpes0_xijet.root sysSummary_pes_down 0 pes_down 0
 ./plot2ff nominal_xijet.root systpes1_xijet.root sysSummary_pes_up 0 pes_up 0
@@ -52,7 +52,7 @@
 ## just trk
 ./ff_systematics.exe gammaxi0_sys.list gammaxi0_sys.root 5
 ./add_sys.exe gammaxi0_sys.root gammaxi0_sys_plot.root
-./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_trk 0 trk
+./plot_ff nominal_xijet.root gammaxi0_sys_plot.root sysSummary_result_trk 0 trk 0
 ./plot_ff_ratio nominal_xijet_ratio.root gammaxi0_sys_plot.root sysSummary_result_ratio_trk trk
 ./plot2ff nominal_xijet.root systtrk_xijet.root sysSummary_trk 0 trk 0
 ./plot2ff nominal_xijet.root systtrk_xijet.root sysSummary_trk_ratio 0 trk 1
