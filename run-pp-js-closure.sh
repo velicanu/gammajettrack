f462b00ddad044d6277fa0c2f7da46776c9c0991
Ran Bi <biran@mit.edu>
2017-04-10 18:02:36 -0400

    update pp js closure script

--- run-pp-js-closure.sh
+++ run-pp-js-closure.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 6 ]; then
   echo "Usage: ./run-pp-js-closure.sh <phoetmin> <phoetmax> <jetptmin> <trkptmin> <gammaxi> <types...>"
-  echo "Example: ./run-pp-js-closure.sh 60 1000 30 1 0 sgengen sgenreco recogen recoreco gen0gen0 recogen0"
+  echo "Example: ./run-pp-js-closure.sh 60 1000 30 1 0 sgengen sgenreco recogen recoreco gengen"
   exit 1
 fi
 
@@ -16,17 +16,17 @@ g++ plot_js.C $(root-config --cflags --libs) -Werror -Wall -O2 -o plot_js || exi
 echo running closure histograms
 for i in ${@:6}
 do
-  ./jetshape /export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170404.root ppmc 0 20 $1 $2 $3 $i $4 $5 &
-  ./jetshape /export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170404.root ppmc 20 60 $1 $2 $3 $i $4 $5 &
-  ./jetshape /export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170404.root ppmc 60 100 $1 $2 $3 $i $4 $5 &
-  ./jetshape /export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170404.root ppmc 100 200 $1 $2 $3 $i $4 $5 &
-  wait
+  ./jetshape /export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170406.root ppmc 0 20 $1 $2 $3 $i $4 $5 &
+  ./jetshape /export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170406.root ppmc 20 60 $1 $2 $3 $i $4 $5 &
+  ./jetshape /export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170406.root ppmc 60 100 $1 $2 $3 $i $4 $5 &
+  ./jetshape /export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170406.root ppmc 100 200 $1 $2 $3 $i $4 $5 &
+done
+wait
 
-  hadd -f ppclosure_${1}_gxi${5}_${i}_js.root ppmc_${i}_${1}_*_*.root
-  rm ppmc_${i}_${1}_*_*.root
+for i in ${@:6}
+do
+  hadd -f closure_${1}_${3}_gxi${5}_${i}_js.root ppmc_${i}_${1}_${3}_*_*.root
+  rm ppmc_${i}_${1}_${3}_*_*.root
 done
 
-hadd -f ppclosure_${1}_gxi${5}_js_merged.root ppclosure_${1}_gxi${5}_*_js.root
-./draw_js ppmc ppclosure_${1}_gxi${5}_js_merged.root ppclosure_${1}_gxi${5}_js_final.root ${@:6}
-
-./run-js-plot.sh $1 $2 $3 $4 $5 ppmc ppclosure ${@:6}
+./run-js-plot.sh $1 $2 $3 $4 $5 ppmc closure ${@:6}
