f7f5a2803397bd5b6264fb3bb8b630edc3d00821
Ran Bi <biran@mit.edu>
2017-04-12 10:31:43 -0400

    update residuals

--- condor/makeresiduals.sh
+++ condor/makeresiduals.sh
@@ -1,5 +1,5 @@
 mkdir test
-for i in `ls | grep -v tmp_run.sh | grep -v .condor | grep -v submit_condor.sh | grep -v skim-pbpbmc.sh | grep -v gammajetSkim.exe | grep -v residuals | grep -v test `
+for i in `ls | grep -v .sh | grep -v .condor | grep -v gammajetSkim.exe | grep -v residuals | grep -v test `
 do
     cp -r $i test
 done
