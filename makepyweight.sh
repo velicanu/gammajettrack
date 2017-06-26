eb72ff101b98df2c80d261961bee821b1aaddcd5
Dragos Velicanu <velicanu@mit.edu>
2017-06-26 10:46:40 -0400

    added make pyreweight

---
+++ makepyweight.sh
@@ -0,0 +1,16 @@
+pho=(60 80)
+jet=(30 40)
+ga=(0 1)
+qg=(1 2)
+
+for pt in `seq 0 1` 
+do  
+    for g in `echo ${ga[*]}` 
+    do
+	for q in `echo ${qg[*]}` 
+	do
+	    ./doclosure.sh ${pho[$pt]} 1000 ${jet[$pt]} ${q} 1 ${g} recoreco gengen0 
+	    mv closure_pbpb_${pho[$pt]}_1000_${jet[$pt]}_gammaxi${g}.root closure_pbpb_${pho[$pt]}_1000_${jet[$pt]}_${q}_gammaxi${g}.root 
+	done
+    done
+done
