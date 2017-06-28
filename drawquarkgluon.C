2473cb701be1aef978644682a7aa079b74cbe5f9
Dragos Velicanu <velicanu@mit.edu>
2017-06-28 15:11:35 -0400

    more histogram renaming

--- drawquarkgluon.C
+++ drawquarkgluon.C
@@ -5,8 +5,8 @@ void drawquarkgluon(int phoetmin, int jetptmin, int icent, int datamc, int gamma
   TCanvas * c1 = new TCanvas();
   myfunc();
   TF1 * fitted = quarkgluon(phoetmin,jetptmin,icent,datamc,gammaxi);
-  TH1D * h22 = (TH1D*)h2->Clone("h22");
-  TH1D * h33 = (TH1D*)h3->Clone("h33");
+  TH1D * hquarktemplate = (TH1D*)h2->Clone("hquarktemplate");
+  TH1D * hgluontemplate = (TH1D*)h3->Clone("hgluontemplate");
   float gluonfraction = fitted->GetParameter(1);
   if(gluonfraction > 1) gluonfraction = 1;
   if(gluonfraction < 0) gluonfraction = 0;
@@ -14,14 +14,14 @@ void drawquarkgluon(int phoetmin, int jetptmin, int icent, int datamc, int gamma
   float scale = fitted->GetParameter(0);
   float error = fitted->GetParError(1);
   
-  h22->Scale(quarkfraction*scale);
-  h33->Scale(gluonfraction*scale);
-  TH1D * hsum = (TH1D*)h22->Clone("hsum");
-  hsum->Add(h33);
-  h22->SetLineColor(kBlue);
-  h33->SetLineColor(kGreen);
-  h33->Draw("hist same");
-  h22->Draw("hist same");
+  hquarktemplate->Scale(quarkfraction*scale);
+  hgluontemplate->Scale(gluonfraction*scale);
+  TH1D * hsum = (TH1D*)hquarktemplate->Clone("hsum");
+  hsum->Add(hgluontemplate);
+  hquarktemplate->SetLineColor(kBlue);
+  hgluontemplate->SetLineColor(kGreen);
+  hgluontemplate->Draw("hist same");
+  hquarktemplate->Draw("hist same");
   hsum->Draw("hist same");
 
   TLegend * leg = new TLegend(0.25,0.6,0.47,0.9);
@@ -31,8 +31,8 @@ void drawquarkgluon(int phoetmin, int jetptmin, int icent, int datamc, int gamma
   leg->SetTextFont(42);
   leg->SetTextSize(leg->GetTextSize()*0.95);
   leg->AddEntry(h1,Form("PbPb %s %d-%d%%",sdatamc[datamc].data(),centmins[icent]/2,centmaxs[icent]/2),"pe");
-  leg->AddEntry(h22,Form("quark fraction %2.1f%% (%2.1f%%)",quarkfraction*100,error*100),"l");
-  leg->AddEntry(h33,Form("gluon fraction %2.1f%% (%2.1f%%)",gluonfraction*100,error*100),"l");
+  leg->AddEntry(hquarktemplate,Form("quark fraction %2.1f%% (%2.1f%%)",quarkfraction*100,error*100),"l");
+  leg->AddEntry(hgluontemplate,Form("gluon fraction %2.1f%% (%2.1f%%)",gluonfraction*100,error*100),"l");
   leg->AddEntry(fitted,Form("fit E^{#gamma}_{T}>%d",phoetmin),"l");
   leg->Draw();
   c1->SaveAs(Form("quarkgluon_%d_%d_1000_%d_gammaxi%d_%d_%d.png",datamc,phoetmin,jetptmin,gammaxi,centmins[icent],centmaxs[icent]));
